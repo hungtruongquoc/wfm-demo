06271579d07a608555121bcf5d6dbb24f5bfca6e
Hung Truong <hungtruongquoc@gmail.com>
2017-11-20 16:35:01 -0600

    Added new property for the global progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgbProgressbarConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Embedded App';
+
+  // Property for showing/hiding the global progress bar
+  protected _progressBarDisplayed = true;
+
+  set progressBarDisplayed(show: boolean) {
+    this._progressBarDisplayed = show;
+  }
+
+  get progressBarDisplayed(): boolean {
+    return this._progressBarDisplayed;
+  }
+
 }

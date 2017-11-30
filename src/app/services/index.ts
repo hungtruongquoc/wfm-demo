9c07f61199e7c57b5adf53d04689064def621cc8
Hung Truong <hungtruongquoc@gmail.com>
2017-11-30 15:15:42 -0600

    Removed all comments

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -8,32 +8,18 @@ export class NiceDataService {
   constructor(private environment: any, private http: HttpClient) {}
 
   private getManagementUnitApiUrl() {
-    // For production, we use the original endpoint
-    // if (this.environment.production) {
-    // return this.environment.apiBaseUrl;
     return 'http://172.25.111.19/TV4/services/rs/mus/selector';
-    // }
-    // Temporary solution used for now as we don't have a standardized endpoint URLs
-    // For development, we attach the string to the base end point
-    // return this.environment.apiBaseUrl + '/mus';
   }
 
   get ManagementUnits() {
     const url = this.getManagementUnitApiUrl();
     const req = new HttpRequest('GET', url, null, {reportProgress: true, responseType: 'json'});
-    // if (this.environment.production) {
-      // Makes request to set the cookies
-      const cookieUrl = 'http://172.25.111.19/delegate/forwarderServlet/process.do?url=http://172.25.111.19/TV4/services/rs/system/config&initpage=http://172.25.111.19/TV4/services/rs/auth/platform/sso&appid=TV4';
-      const cookieReq = new HttpRequest('GET', cookieUrl);
-
-      return this.http.request(cookieReq).flatMap((event) => {
-        if (event.type === HttpEventType.Response) {
-          return this.http.request(req);
-        }
-      });
-    // }
-    // else {
-    //   return this.http.request(req);
-    // }
+    const cookieUrl = 'http://172.25.111.19/delegate/forwarderServlet/process.do?url=http://172.25.111.19/TV4/services/rs/system/config&initpage=http://172.25.111.19/TV4/services/rs/auth/platform/sso&appid=TV4';
+    const cookieReq = new HttpRequest('GET', cookieUrl);
+    return this.http.request(cookieReq).flatMap((event) => {
+      if (event.type === HttpEventType.Response) {
+        return this.http.request(req);
+      }
+    });
   }
 }

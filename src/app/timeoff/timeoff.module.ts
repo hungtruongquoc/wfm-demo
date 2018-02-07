02a3e36001df236acf046d5bcb1fc506a5f76338
hungtruongquoc <hungtruongquoc@gmail.com>
2018-02-07 11:53:58 -0600

    Added a route to index component of time-off module

--- src/app/timeoff/timeoff.module.ts
+++ src/app/timeoff/timeoff.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
+import {TimeOffRoutingModule} from './timeoff-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TimeOffRoutingModule
   ],
   declarations: [IndexComponent]
 })

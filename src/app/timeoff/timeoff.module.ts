793afa33c2a678379fa94142fbb23ca86b37d4a7
hungtruongquoc <hungtruongquoc@gmail.com>
2018-02-07 11:46:44 -0600

    Added index component for time-off module

--- src/app/timeoff/timeoff.module.ts
+++ src/app/timeoff/timeoff.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [IndexComponent]
 })
 export class TimeOffModule { }

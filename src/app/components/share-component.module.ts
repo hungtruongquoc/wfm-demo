b944280056f84093e29547dba4a443fbc73b56ca
hungtruongquoc <hungtruongquoc@gmail.com>
2018-02-07 18:30:46 -0600

    Completed the table for displaying data

--- src/app/components/share-component.module.ts
+++ src/app/components/share-component.module.ts
@@ -5,13 +5,20 @@ import {BaseComponent} from './base.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoadingComponent } from './loading/loading.component';
 import {BaseSmartComponent} from './base-smart.component';
-import {MatDividerModule, MatTableModule} from '@angular/material';
+import {
+  MatDividerModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule,
+  MatTableModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     NgbModule,
     MatDividerModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [
     BaseComponent,
@@ -25,7 +32,11 @@ import {MatDividerModule, MatTableModule} from '@angular/material';
     BaseComponent,
     BaseSmartComponent,
     MatDividerModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 
 })
